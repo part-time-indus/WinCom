aa1d2c737eaf779376685cfe12cbe7a37aa80daf
Vik Bhullar <epic_indus_dev@gmx.com>
2023-04-21 17:21:06 +1000

    component signature

---
+++ src/com_component2/cmpnt2.cpp
@@ -0,0 +1,28 @@
+#include<objbase.h>
+
+
+interface IX: public IUnknown{
+    virtual void Fx() = 0;
+};
+
+interface IY: public IUnknown{
+    virtual void Fy() = 0;
+
+};
+
+
+class CA: public IX, public IY{
+    public:
+        CA();
+        ~CA();
+        virtual HRESULT QueryInterface(IID& iid, void** ppv);
+        virtual ULONG AddRef();
+        virtual ULONG Release();
+        virtual void Fx();
+        virtual void Fy();
+    private:
+        int sm_ref;
+};
+
+
+
