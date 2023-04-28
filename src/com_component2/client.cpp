179b0b2c01712f26b0c81c3ce5a9982d50a99f8f
Vik Bhullar <epic_indus_dev@gmx.com>
2023-04-28 18:20:00 +1000

    E: 0x800401F0. CoInitialize not called . Fixed

--- src/com_component2/client.cpp
+++ src/com_component2/client.cpp
@@ -1,35 +1,24 @@
 #include<objbase.h>
 #include<iostream>
 #include "Iface.h"
-
-errno_t ConvertCLSID(CLSID&, char*, int);
+#include "assert.h"
+using namespace std;
 
 void trace(const char* msg){
     std::cout << msg << std::endl;
 }
 
-errno_t ConvertCLSID(const CLSID& clsid, char* szCLSID, int length){
-    HRESULT hr;
-    LPOLESTR wszCLSID = NULL;
-    errno_t wsz_to_sz;
-    hr = StringFromCLSID(clsid, &wszCLSID);
-    // assert(SUCCEEDED(hr));
-    if(SUCCEEDED(hr)){
-        size_t converted;
-        wsz_to_sz = wcstombs_s(&converted, szCLSID, length, wszCLSID, length);
-        std::cout <<"Bytes in szCLSID:\t" << sizeof(szCLSID) << std::endl;
-        std::cout << "Wide char string\t" << wszCLSID << std::endl;
-        std::cout << "Size of wide\t" << wcslen(wszCLSID) << std::endl;
-        std::cout<< "Number of characters converted:\t" << converted << std::endl;
-    }
-
-    CoTaskMemFree(wszCLSID);
-    return wsz_to_sz;
-
-}
 int main(){
-    char szClsid[39];
-    errno_t  err = ConvertCLSID(CLSID_Component1, szClsid, 39);
-    std::cout << szClsid << std::endl;
+    CoInitialize(NULL);
+    HRESULT hr;
+    IX* pIX = NULL;
+    hr = CoCreateInstance(CLSID_Component1, NULL, CLSCTX_INPROC_SERVER, IID_IX, (void**) &pIX);
+    if(SUCCEEDED(hr)){
+        trace("Created object");
+        pIX->Fx();
+        pIX->Release();
+    }
+    CoUninitialize();
+    return(0);
 
 }
