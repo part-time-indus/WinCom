ea9f0596d8c867a18589e45180a42e1a795c5529
Vik Bhullar <epic_indus_dev@gmx.com>
2023-05-05 20:05:50 +1000

    Create Registry and Set Value

--- src/com_component3/registry.cpp
+++ src/com_component3/registry.cpp
@@ -16,3 +16,44 @@ extern "C" void CLSIDtoChar(const CLSID& clsid, char* szCLSID, int length){
     errno_t err = wcstombs_s(&sz_converted, szCLSID, length, wszCLSID, length);
     assert(err == 0);
 }
+
+extern "C" BOOL CreateKey(const char* key, const char* subkey, const char* value){
+    char szKey[1024];
+    size_t key_len = 1024;
+    errno_t eResult = strcpy_s(szKey, key_len, key);
+    assert(eResult == 0);
+    eResult = strcat_s(szKey, key_len, "\\");
+    assert(eResult == 0);
+    if(subkey != NULL){
+        eResult = strcat_s(szKey, 64, subkey);
+        assert(eResult == 0);
+
+    }
+    HKEY hKey;
+    LSTATUS lResult = RegCreateKeyExA(
+        HKEY_CLASSES_ROOT,
+        szKey,
+        0,
+        NULL,
+        REG_OPTION_NON_VOLATILE,
+        KEY_ALL_ACCESS,
+        NULL,
+        &hKey,
+        NULL
+    ); 
+    if(lResult != ERROR_SUCCESS){
+        return false;
+    }
+
+    if(value != NULL){
+        LSTATUS lResult = RegSetValueExA(hKey,
+                                        NULL,
+                                        0,
+                                        REG_SZ,
+                                        (BYTE*) value,
+                                        strlen(value) + 1
+                                        );
+    }
+    RegCloseKey(hKey);
+    return true;
+}
--- src/com_component3/registry.h
+++ src/com_component3/registry.h
@@ -1,2 +1,3 @@
 #include<objbase.h>
 extern "C" void CLSIDtoChar(const CLSID&, char*, int);
+extern "C" HRESULT CreateKey(const char*, const char*, const char*);
