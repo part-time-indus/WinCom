1a9fc140dbe9ebc2d519fdb8524b82406864ec42
Vik Bhullar <epic_indus_dev@gmx.com>
2023-05-05 18:43:24 +1000

    add return

--- src/com_component3/client.cpp
+++ src/com_component3/client.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 #include "iface.h"
 #include "registry.h"
+
+
 int main(){
     const int szCLSIDlen = 39;
     char szCLSID[szCLSIDlen];
     CLSIDtoChar(CLSID_COMPONENT1, szCLSID, szCLSIDlen);
     std::cout << "CLSID char:\t" << szCLSID << std::endl; 
+    return(0);
 }
