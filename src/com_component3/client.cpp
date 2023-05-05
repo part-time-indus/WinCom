ec637fd680b1b8d26fa0928fa742ab31acab22ab
Vik Bhullar <epic_indus_dev@gmx.com>
2023-05-05 17:08:52 +1000

    debug clsidtochar

--- src/com_component3/client.cpp
+++ src/com_component3/client.cpp
@@ -1,3 +1,8 @@
+#include<iostream>
+#include "registry.h";
 int main(){
-    
+    const int szCLSIDlen = 39;
+    char szCLSID[szCLSIDlen];
+    CLSIDtoChar(CLSID_COMPONENT1, szCLSID, szCLSIDlen);
+    std::cout << "CLSID char:\t" << szCLSID << std::endl; 
 }
