dc3fe9ba42a58c1ba559f1c5bcb9aa4cf0358d42
Vik Bhullar <epic_indus_dev@gmx.com>
2023-05-05 17:08:16 +1000

    string for a given CLSID

---
+++ src/com_component3/registry.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<objbase.h>
+#include<assert.h>
+
+
+void trace(char* pMsg){
+    std::cout << pMsg << std::endl;
+}
+
+
+
+void CLSIDtoChar(const CLSID& clsid, char* szCLSID, int length){
+    LPOLESTR wszCLSID;
+    HRESULT hr = StringFromCLSID(clsid,&wszCLSID);
+    assert(hr == S_OK);
+    int sz_converted; 
+    errno_t err = wctomb_s(&sz_converted, szCLSID, length, *wszCLSID);
+    assert(err == 0);
+}
---
+++ src/com_component3/registry.h
@@ -0,0 +1,2 @@
+#include<objbase.h>
+extern "C" void CLSIDtoChar(const CLSID&, char*, int);
