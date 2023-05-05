8e1fcbb0a05ee4e7c366846befe2e1ce472c719e
Vik Bhullar <epic_indus_dev@gmx.com>
2023-05-06 00:53:02 +1000

    change return to bool

--- src/com_component3/registry.h
+++ src/com_component3/registry.h
@@ -1,3 +1,3 @@
 #include<objbase.h>
 extern "C" void CLSIDtoChar(const CLSID&, char*, int);
-extern "C" HRESULT CreateKey(const char*, const char*, const char*);
+extern "C" BOOL CreateKey(const char*, const char*, const char*);
