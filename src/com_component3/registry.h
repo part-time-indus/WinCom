5c45c98a2d51a46128d21a848c7915c03c3336f9
Vik Bhullar <epic_indus_dev@gmx.com>
2023-05-08 14:56:16 +1000

    moved local function definitions. fixed typos

--- src/com_component3/registry.h
+++ src/com_component3/registry.h
@@ -1,6 +1,3 @@
 #include<objbase.h>
-void CLSIDtoChar(const CLSID&, char*, int);
-BOOL CreateKey(const char*, const char*, const char*);
-LONG RecursiveDeleteKey(const char*, const char*);
-extern "C" HRESULT RegisterDLL(HMODULE, const CLSID&, const char*, const char*, const char*);
-extern "C" HRESULT UnregisterDLL(const CLSID&, const char*, const char*);
+extern "C" HRESULT RegisterDll(HMODULE, const CLSID&, const char*, const char*, const char*);
+extern "C" HRESULT UnregisterDll(const CLSID&, const char*, const char*);
