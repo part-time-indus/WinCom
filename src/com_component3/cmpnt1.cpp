fb7aecdaed37c4262c257770a52f0ad4afd5a103
Vik Bhullar <epic_indus_dev@gmx.com>
2023-05-08 13:25:13 +1000

    Factory constructor

--- src/com_component3/cmpnt1.cpp
+++ src/com_component3/cmpnt1.cpp
@@ -5,6 +5,7 @@
 #define global static
 
 global long g_cComponents = 0;
+global long g_cServerLocks = 0;
 
 void trace(char* pMsg){
     std::cout << pMsg << std::endl;
@@ -14,11 +15,11 @@ class CA: public IX, public IY{
     public:
         CA();
         ~CA();
-        virtual HRESULT __stdcall QueryInterface(const IID& iid, void** ppv) = 0;
-        virtual ULONG __stdcall AddRef() = 0;
-        virtual ULONG __stdcall Release() = 0;
-        virtual void Fx() = 0;
-        virtual void Fy() = 0;
+        virtual HRESULT __stdcall QueryInterface(const IID& iid, void** ppv);
+        virtual ULONG __stdcall AddRef();
+        virtual ULONG __stdcall Release();
+        virtual void Fx();
+        virtual void Fy();
     private: 
         long m_cRef;
 };
@@ -29,7 +30,7 @@ CA::CA(): m_cRef(1){
 }
 
 CA::~CA(){
-    trace("Destroying itself");
+    trace("Component:\tDestroying itself");
     InterlockedDecrement(&g_cComponents);
 }
 HRESULT __stdcall CA::QueryInterface(const IID& iid, void** ppv){
@@ -38,8 +39,10 @@ HRESULT __stdcall CA::QueryInterface(const IID& iid, void** ppv){
     if(iid == IID_IUnknown){
         *ppv = static_cast<IX*>(this);
     }else if(iid == IID_IX){
+        trace("Component:\tReturn pointer to IX");
         *ppv = static_cast<IX*>(this);
     }else if(iid == IID_IY){
+        trace("Component:\tReturn to pointer IY");
         *ppv = static_cast<IY*>(this);
     }else{
         return E_NOINTERFACE;
@@ -71,4 +74,70 @@ void CA::Fy(){
 
 
 
+class CFactory: public IClassFactory{
+    public:
+        CFactory();
+        ~CFactory();
+        virtual HRESULT __stdcall QueryInterface(const IID& iid, void** ppv);
+        virtual ULONG __stdcall AddRef();
+        virtual ULONG __stdcall Release();
+        virtual HRESULT __stdcall CreateInstance(IUnknown* pUnkOuter, const IID& iid, void** ppv);
+        virtual HRESULT __stdcall LockServer(BOOL bLock);
+    private:
+        long m_cRef;
+};
 
+CFactory::CFactory():m_cRef(1){
+
+}
+
+CFactory::~CFactory(){
+    trace("Class factory:\t\tDestroy self.");
+}
+
+HRESULT __stdcall CFactory::QueryInterface(const IID& iid, void** ppv){
+    *ppv = NULL;
+    if(iid == IID_IUnknown || iid == IID_IClassFactory){
+        *ppv = static_cast<IClassFactory*>(this);
+    }else{
+        return E_NOINTERFACE;
+    }
+    static_cast<IUnknown*>(this)->AddRef();
+}
+
+ULONG __stdcall CFactory::AddRef(){
+    return InterlockedIncrement(&m_cRef);
+}
+
+ULONG __stdcall CFactory::Release(){
+    InterlockedDecrement(&m_cRef);
+    if(m_cRef == 0){
+        delete this;
+        m_cRef = 0;
+    }
+    return m_cRef;
+}
+
+HRESULT __stdcall CFactory::CreateInstance(IUnknown* pUnkOuter, const IID& iid, void** ppv){
+    trace("Class Factory:\t\tCreate Component");
+    if(pUnkOuter != NULL){
+        return CLASS_E_NOAGGREGATION;
+    }
+    CA* ca = new CA;
+    if(ca == NULL){
+        return E_OUTOFMEMORY;
+
+    }
+    HRESULT hr = ca->QueryInterface(iid, ppv);
+    ca->Release();
+    return hr;
+}
+
+HRESULT __stdcall CFactory::LockServer(BOOL bLock){
+    if(bLock){
+        InterlockedIncrement(&g_cServerLocks);
+    }else{
+        InterlockedDecrement(&g_cServerLocks);
+    }
+    return S_OK;
+}
