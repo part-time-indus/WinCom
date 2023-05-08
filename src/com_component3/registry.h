a763991f58935fee2f497e4cb1a383a28b7c800a
Vik Bhullar <epic_indus_dev@gmx.com>
2023-05-08 11:29:43 +1000

    for internal use in the dll only.

--- src/com_component3/registry.h
+++ src/com_component3/registry.h
@@ -1,6 +1,6 @@
 #include<objbase.h>
-extern "C" void CLSIDtoChar(const CLSID&, char*, int);
-extern "C" BOOL CreateKey(const char*, const char*, const char*);
-extern "C" LONG RecursiveDeleteKey(const char*, const char*);
+void CLSIDtoChar(const CLSID&, char*, int);
+BOOL CreateKey(const char*, const char*, const char*);
+LONG RecursiveDeleteKey(const char*, const char*);
 extern "C" HRESULT RegisterDLL(HMODULE, const CLSID&, const char*, const char*, const char*);
 extern "C" HRESULT UnregisterDLL(const CLSID&, const char*, const char*);
