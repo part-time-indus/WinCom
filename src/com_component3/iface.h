1fd906bb1ae970ebf49d7b5d9af1ff76f8ff130b
Vik Bhullar <epic_indus_dev@gmx.com>
2023-05-08 11:35:43 +1000

    interfaces components implement

--- src/com_component3/iface.h
+++ src/com_component3/iface.h
@@ -1,4 +1,17 @@
 #include<objbase.h>
+
+interface IX: public IUnknown{
+    virtual void Fx() = 0;
+};
+
+interface IY: public IUnknown{
+    virtual void Fy() = 0;
+};
+
+interface IZ: public IUnknown{
+    virtual void Fz() = 0;
+};
+
 extern "C" CLSID CLSID_COMPONENT1;
 extern "C" CLSID CLSID_COMPONENT2;
 extern "C" IID IID_IX;
