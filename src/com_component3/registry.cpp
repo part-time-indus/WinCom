ee97d2acbb46160fc6acda8f28fd4e56a723bca1
Vik Bhullar <epic_indus_dev@gmx.com>
2023-05-08 14:56:57 +1000

    move utility function signatures

--- src/com_component3/registry.cpp
+++ src/com_component3/registry.cpp
@@ -2,12 +2,11 @@
 #include<objbase.h>
 #include<assert.h>
 
+void CLSIDtoChar(const CLSID&, char*, int);
+BOOL CreateKey(const char*, const char*, const char*);
+LONG RecursiveDeleteKey(HKEY key, const char*);
 
 const int CLSID_STRLEN = 39;
-
-void trace(char* pMsg){
-    std::cout << pMsg << std::endl;
-}
 
 extern "C" HRESULT RegisterDll(HMODULE hModule, const CLSID& clsid, const char* szFriendlyName, const char* szVerIndProgName, const char* szProgName){
     char szCLSID[CLSID_STRLEN];
@@ -16,29 +15,29 @@ extern "C" HRESULT RegisterDll(HMODULE hModule, const CLSID& clsid, const char* 
     CLSIDtoChar(clsid, szCLSID, CLSID_STRLEN);
     errno_t eRes = strcpy_s(szKey,64,"CLSID\\");
     assert(eRes == 0);
-    errno_t eRes = strcat_s(szKey, 64, szCLSID);
+    eRes = strcat_s(szKey, 64, szCLSID);
     assert(eRes == 0);
     DWORD hRes = GetModuleFileNameA(hModule, szFile, 512);
     assert(hRes != 0);
     
     BOOL bRes = CreateKey(szKey, NULL, szFriendlyName);
-    assert(bRes == true);
+    assert(bRes == TRUE);
     bRes = CreateKey(szKey, "InProcServer32", szFile);
-    assert(bRes == true);
+    assert(bRes == TRUE);
     bRes = CreateKey(szKey, "ProgId", szProgName);
-    assert(bRes == true);
+    assert(bRes == TRUE);
     
     bRes = CreateKey(szProgName, NULL, szFriendlyName);
-    assert(bRes == true);
+    assert(bRes == TRUE);
     bRes = CreateKey(szProgName, "CLSID", szCLSID);
-    assert(bRes == true);
+    assert(bRes == TRUE);
 
     bRes = CreateKey(szVerIndProgName, NULL, szFriendlyName);
-    assert(bRes == true);
+    assert(bRes == TRUE);
     bRes = CreateKey(szVerIndProgName, "CLSID", szCLSID);
-    assert(bRes == true);
+    assert(bRes == TRUE);
     bRes = CreateKey(szVerIndProgName, "CurrVer", szProgName);
-    assert(bRes == true);
+    assert(bRes == TRUE);
     return S_OK;
 
 }
@@ -65,7 +64,7 @@ extern "C" HRESULT UnregisterDll(const CLSID& clsid, const char* szProgName, con
 
 
 
-extern "C" void CLSIDtoChar(const CLSID& clsid, char* szCLSID, int length){
+void CLSIDtoChar(const CLSID& clsid, char* szCLSID, int length){
     LPOLESTR wszCLSID;
     HRESULT hr = StringFromCLSID(clsid,&wszCLSID);
     assert(hr == S_OK);
@@ -75,7 +74,7 @@ extern "C" void CLSIDtoChar(const CLSID& clsid, char* szCLSID, int length){
     CoTaskMemFree(wszCLSID);
 }
 
-extern "C" BOOL CreateKey(const char* key, const char* subkey, const char* value){
+BOOL CreateKey(const char* key, const char* subkey, const char* value){
     char szKey[1024];
     size_t dwSize = 1024;
     errno_t eResult = strcpy_s(szKey, dwSize, key);
@@ -117,7 +116,7 @@ extern "C" BOOL CreateKey(const char* key, const char* subkey, const char* value
 }
 
 
-extern "C" LONG RecursiveDeleteKey(HKEY hKey, const char* subkey){
+LONG RecursiveDeleteKey(HKEY hKey, const char* subkey){
     HKEY szKey;
     LSTATUS lResult = RegOpenKeyExA(hKey, subkey, 0, KEY_ALL_ACCESS, &szKey);
     if(lResult != ERROR_SUCCESS){
@@ -125,7 +124,7 @@ extern "C" LONG RecursiveDeleteKey(HKEY hKey, const char* subkey){
     }
     char szSubkey[256];
     DWORD dwSize = 256;
-    FILETIME* lpLastWriteTime;
+    FILETIME* lpLastWriteTime = NULL;
     while(RegEnumKeyExA(szKey, 0, szSubkey,&dwSize,NULL,NULL,NULL,lpLastWriteTime) == ERROR_SUCCESS){
         lResult = RecursiveDeleteKey(szKey, szSubkey);
         if(lResult != ERROR_SUCCESS){
