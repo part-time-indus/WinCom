8a158fe828a2891ba5a82d5f4648f08717c77d33
Vik Bhullar <epic_indus_dev@gmx.com>
2023-05-08 14:57:56 +1000

    clsidtochar not callable from dll.

--- src/com_component3/client.cpp
+++ src/com_component3/client.cpp
@@ -4,9 +4,5 @@
 
 
 int main(){
-    const int szCLSIDlen = 39;
-    char szCLSID[szCLSIDlen];
-    CLSIDtoChar(CLSID_COMPONENT1, szCLSID, szCLSIDlen);
-    std::cout << "CLSID char:\t" << szCLSID << std::endl; 
     return(0);
 }
