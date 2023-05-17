641692ddae9938d02cf9e1d903294341bfca7ed3
Vik Bhullar <epic_indus_dev@gmx.com>
2023-05-17 11:54:30 +1000

    client using components

--- src/com_component3/client.cpp
+++ src/com_component3/client.cpp
@@ -1,8 +1,53 @@
 #include<iostream>
 #include "iface.h"
 #include "registry.h"
+#include <objbase.h>
 
 
-int main(){
-    return(0);
+using namespace std;
+
+void trace(const char* msg) { cout << "Client: \t" << msg << endl ;}
+
+//
+// main function
+//
+int main()
+{
+	// Initialize COM Library
+	CoInitializeEx(NULL,COINIT_APARTMENTTHREADED) ;
+	
+	trace("Get interface IY from Component 1.") ;
+	IY* pIY = NULL ; 
+	HRESULT hr = ::CoCreateInstance(CLSID_COMPONENT1,
+	                                NULL, 
+	                                CLSCTX_INPROC_SERVER,
+	                                IID_IY, 
+	                                (void**)&pIY) ;
+	if (SUCCEEDED(hr))
+	{
+	   IZ* pIZ = NULL;
+	   pIY->Fy();
+	   hr = pIY->QueryInterface(IID_IZ, (void**)&pIZ);
+	   if(SUCCEEDED(hr)){
+	   	pIZ->Fz();
+		IX* pIX = NULL;
+		hr = pIZ->QueryInterface(IID_IX, (void**)&pIX);
+		if(SUCCEEDED(hr)){
+			pIX->Fx();
+	   		pIX->Release();
+		}
+		pIZ->Release();
+	   }
+
+
+	}
+	else
+	{
+		cout << "Could not create component: " << hex << hr << endl ;
+	}
+	pIY->Release();
+	
+	CoUninitialize();
+	std::cout << "Exiting Main";
+	return 0 ;
 }
