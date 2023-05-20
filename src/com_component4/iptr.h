95b5fb47a89e5d6581e971774f62102be9d7b3eb
Vik Bhullar <epic_indus_dev@gmx.com>
2023-05-20 15:15:26 +1000

    constructors

--- src/com_component4/iptr.h
+++ src/com_component4/iptr.h
@@ -2,6 +2,21 @@
 template<class T, const IID* iid> 
 class IPtr
 {
+    public:
+        IPtr(){
+            m_pI = NULL;
+        }
+        IPtr(T* pI){
+            m_pI = pI;
+            if(m_pI != NULL){
+                m_pI->AddRef();
+            }
+        }
+        IPtr(IUnknown* pI){
+            if(pI != NULL){
+                HRESULT hr = pI->QueryInterface(iid, (void**)&m_pI);
+            }
+        }
 
         
     private: 
